f6435d4d078f8fca77a38b7d3dc99df0a05e5409
João Gabriel <jgab2005@live.com>
2025-10-15 10:46:33 -0300

    reajustei

--- MottuFlow/webapp.sh
+++ MottuFlow/webapp.sh
@@ -6,6 +6,8 @@ APP_NAME="mottuflow-app"
 PLAN_NAME="mottuflow-plan"
 LOCATION="brazilsouth"
 
+az group show --name "$RG" >/dev/null 2>&1 || az group create --name "$RG" --location "$LOCATION"
+
 az appservice plan show --name "$PLAN_NAME" --resource-group "$RG" >/dev/null 2>&1 || \
 az appservice plan create \
   --name "$PLAN_NAME" \
@@ -18,6 +20,7 @@ az webapp create \
   --resource-group "$RG" \
   --plan "$PLAN_NAME" \
   --name "$APP_NAME" \
-  --runtime "JAVA|21-java21"
+  --runtime "JAVA|21-java21" \
+  --deployment-local-git
 
 echo "Web App pronto: $APP_NAME"
